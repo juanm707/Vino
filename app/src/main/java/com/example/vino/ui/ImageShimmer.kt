42fbf7439bc6c7d5e23a172c7afa713ec9e4c387
Juan Martinez <juanm.jm49@gmail.com>
2021-08-12 21:16:57 -0700

    gifs

--- app/src/main/java/com/example/vino/ui/ImageShimmer.kt
+++ app/src/main/java/com/example/vino/ui/ImageShimmer.kt
@@ -3,9 +3,9 @@ package com.example.vino.ui
 import com.facebook.shimmer.Shimmer
 
 class ImageShimmer {
-    val shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
+    val shimmer: Shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
         .setDuration(1200) // how long the shimmering animation takes to do one full sweep
-        .setBaseAlpha(0.8f) //the alpha of the underlying children
+        .setBaseAlpha(1f) //the alpha of the underlying children
         .setHighlightAlpha(0.9f) // the shimmer alpha amount
         .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
         .setAutoStart(true)
